efd28c1b82c196bed7e76d746db7852bcd464290
yiqi0110 <lili-rovert@hotmail.com>
2018-08-03 14:03:49 -0600

    basically trashed my original attempt and am using this, much easier way... thus far.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,60 @@
 // this is a list of the nigiri
-var nigiriList = ["shake", "maguro", "hamachi", "tai", "toro", "akami", "tako", "uni", "akamutsu", "kodai"]
+var nigiriList = ["shake", "maguro", "hamachi", "tai", "toro", "akami", "tako", "uni", "akamutsu", "kodai"];
 
 // randomizes the array for the game
-nigiriList.sort(function () {
-    return 0.9 - Math.random()
-});
-for (var i = 0; i < nigiriList.length; i++) {
+var randomNigiri = nigiriList[Math.floor(Math.random() * nigiriList.length)];
 
-    // To get the array to output the individual letters for each array index
-    var listLetters = nigiriList[i].split('');
-    for (var j = 0; j < listLetters.length; j++) {
-        // console.log(listLetters[j])     test to get each letter out of array value
-        // var x = document.getElementById("_").placeholder;
-        // document.getElementsByName("_placeholders").item = x;
-        // console.log(x)           IGNOR FOR NOW
+var s;
+var winCount = 0;
+var answerArray = [];
+
+function startUp() {
+    for (var i = 0; i < randomNigiri.length; i++) {
+        answerArray[i] = "_";
+    }
+
+    s = answerArray.join(" ");
+    document.getElementById("_placeholders").innerHTML = s;
+}
+
+function Letter() {
+    // so how intigrate this with the onclick thing so it reads it inplicitly
+    var letter = document.getElementById("letter").value;
+
+    if (letter.length = 1) {
+        for (var i = 0; i < randomNigiri.length; i++) {
+            if (randomNigiri[i] === letter) {
+                answerArray[i] = letter;
+            }
+        }
+        winCount++;
+        document.getElementById("wins").innerHTML = count;
+
+        // this is also linked to the onclick fucntion
+        document.getElementById("_placeholders").innerHTML = answerArray.join(" ");
+    }
+    if (winCount > 10) {
+        // this should show the guessed letters in an array type format
+
+        for (var i = 10; i < 10; i -= i - 1) {
+            var remaingGuesses = i
+        }
+
+        document.getElementById("guesses").innerHTML = remaingGuesses;
     }
 }
+
+// for (var i = 0; i < nigiriList.length; i++) {
+
+// // To get the array to output the individual letters for each array index
+// var listLetters = nigiriList[i].split('');
+// for (var j = 0; j < listLetters.length; j++) {
+// // console.log(listLetters[j])     test to get each letter out of array value
+// var x = document.getElementById("_").placeholder;
+// document.getElementsByName("_placeholders").item = x;
+// console.log(x)           IGNOR FOR NOW
+// }
+// }
 
 // this is for the place holders, class being called "_placeholders"
 
@@ -23,60 +62,58 @@ for (var i = 0; i < nigiriList.length; i++) {
 // console.log(listLetters)
 
 // this is where the user will input a letter
-document.onkeyup = function (event) {
+// document.onkeyup = function (event) {
 
-    // this is to make any input lower case to fit in to the for statement, the variable for user input is x
-    var x = event.key.toLowerCase();
-
-
-
-    // this is where the game starts
-    var first = "a",
-        last = "z";
-    for (var i = first.charCodeAt(0); i <= last.charCodeAt(0); i++) {
-        var alphabet = String.fromCharCode(i);
-
-        // This is saying that if you press any letter the game will initiate
-        if (x === alphabet) {
-
-            var guessesRemaining = -8;
-
-            
+//     // this is to make any input lower case to fit in to the for statement, the variable for user input is x
+//     var x = event.key.toLowerCase();
 
 
 
-            // if user input is x, then replace placeholder "_" with letter, if not place x in wrong letter place
-            if (listLetters.includes(x)) {
-                var wins = 0;
-                // this is where the place holder comes into play
+//     // this is where the game starts
+//     var first = "a",
+//         last = "z";
+//     for (var i = first.charCodeAt(0); i <= last.charCodeAt(0); i++) {
+//         var alphabet = String.fromCharCode(i);
 
-                wins++;
-                document.getElementById("wins").innerHTML = wins;
-                console.log(x);
-            } else {
-                d3.select("button").on("click", function () {
-                    var guessed = [];
-                    d3.selectAll("input[type=character]").each(function () {
-                        inputs.push(this.value);
-                        document.getElementById("guesses").innerHTML = guessed;
-                        console.log(guessed);
-                    })
+//         // This is saying that if you press any letter the game will initiate
+//         if (x === alphabet) {
 
-                    console.log(inputs);
-                    
-                })
+//             var guessesRemaining = -8;
 
-                document.getElementById("remaining").innerHTML = guessesRemaining;
-                guessesRemaining++;
-            }
-            // console.log("some shit happens")        dubugging test
-            console.log(guessesRemaining + "guess");
-        };
-        // console.log(i)      debugging test
 
-        // console.log("hello")        debugging test
-    }
-}
+
+
+
+//             // if user input is x, then replace placeholder "_" with letter, if not place x in wrong letter place
+//             if (listLetters.includes(x)) {
+//                 var wins = 0;
+//                 // this is where the place holder comes into play
+
+//                 wins++;
+//                 document.getElementById("wins").innerHTML = wins;
+//                 console.log(x);
+//             } else {
+//                 var userText = document.getElementById("_placeholders");
+
+//                 document.onkeyup = function(event) {
+//                     userText.textContent = event.key;
+//                 }
+
+
+
+
+
+//                 document.getElementById("remaining").innerHTML = guessesRemaining;
+//                 guessesRemaining++;
+//             }
+//             // console.log("some shit happens")        dubugging test
+//             console.log(guessesRemaining + "guess");
+//         };
+//         // console.log(i)      debugging test
+
+//         // console.log("hello")        debugging test
+//     }
+// }
 
 
 // THIS BELOW IS THE INTIAL PSEUDOCODE THAT DID NOT GO INTO THE ACTUAL CODE
